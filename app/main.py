055a2946429434e26b09445bf5984268e6d9bdd6
johncollinsai <climatemetrics@gmail.com>
2023-04-23 10:28:17 +0800

    v0.2.3, removing text-davinci-003

--- app/main.py
+++ app/main.py
@@ -2,7 +2,7 @@ import random
 
 from flask import Blueprint, render_template, request, jsonify
 # dot (.) before completions, tells Python to import completions module from same directory as main.py
-from .completions import text_davinci_reponse, generate_gpt4_response, get_api_key
+from .completions import generate_gpt4_response, get_api_key
 
 bp = Blueprint("main", __name__)
 
@@ -11,9 +11,8 @@ def index():
     if request.method == "POST":
         prompt = request.form["prompt"]
         api_key = get_api_key()
-        text_davinci = text_davinci_reponse(prompt, api_key)
         gpt4 = generate_gpt4_response(prompt, api_key)
-        return jsonify({"text_davinci": text_davinci, "gpt4": gpt4})
+        return jsonify({"gpt4": gpt4})
     return render_template("index.html")
 
 @bp.route('/predefined')
@@ -23,8 +22,10 @@ def predefined():
         "Example question: Is Tesla sanctioned?",
         "Example question: Does Apple have large cash reserves?",
         "Example question: Does Alibaba have Cayman Island registered subsidiaries?",
-        "Example question: How strong is Microsoft's balance sheet?",
+        "Example question: How strong is Microsoft balance sheet?",
     ]
     question = random.choice(questions)
     return render_template('index.html', predefined_question=question)
+
+
 
