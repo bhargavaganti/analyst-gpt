e3ec0776cc01dc343fac30dd7740c772af654953
johncollinsai <climatemetrics@gmail.com>
2023-04-25 18:11:06 +0800

    v0.2.10, trying to improve formats of responses

--- app/completions.py
+++ app/completions.py
@@ -11,12 +11,52 @@ api_key = get_api_key()
 
 # Modality: Business Analyst
 intro_sentence = "As a business analyst, I have identified the following key insights:"
+bullet_points = [
+    "• Bullet point 1",
+    "• Bullet point 2",
+    "• Bullet point 3",
+    "• Bullet point 4",
+    "• Bullet point 5",
+    "• Bullet point 6",
+    "• Bullet point 7",
+    "• Bullet point 8",
+    "• Bullet point 9",
+    "• Bullet point 10",
+]
 
 # Modality: Investigator
 intro_sentence_investigator = "As an investigator, I have compiled the following data:"
+table_investigator = """
+| General Investigative Information | Corporate Information | Compliance Information |
+|-----------------------------------|------------------------|------------------------|
+| • Sentence 1                     | • Sentence 1           | • Sentence 1           |
+| • Sentence 2                     | • Sentence 2           | • Sentence 2           |
+| • Sentence 3                     | • Sentence 3           | • Sentence 3           |
+"""
 
 # Modality: Financial Analyst
 intro_sentence_financial = "As a financial analyst, I have analyzed the financial aspects of the prompt:"
+table_financial = """
+| Assets & Liabilities          | Cashflows & Liquidity      | Key Financial Ratios     |
+|-------------------------------|-----------------------------|--------------------------|
+| • Sentence 1                 | • Sentence 1                | • Ratio 1                |
+| • Sentence 2                 | • Sentence 2                | • Ratio 2                |
+| • Sentence 3                 | • Sentence 3                | • Ratio 3                |
+"""
+
+def format_business_analyst_response(response_text):
+    formatted_response = response_text.replace("•", "\n•")
+    return formatted_response
+
+def format_investigator_response(response_text):
+    formatted_response = response_text.replace("| ", "").replace(" |", "").replace(" | ", "\n")
+    return formatted_response
+
+def format_financial_response(response_text):
+    formatted_response = response_text.replace("| ", "").replace(" |", "").replace(" | ", "\n")
+    return formatted_response
+
+
 
 def generate_gpt4_response(prompt, modality, api_key):
     if modality == "business analyst":
@@ -41,8 +81,15 @@ def generate_gpt4_response(prompt, modality, api_key):
     )
 
     formatted_response = response.choices[0]["message"]["content"]
-    return formatted_response
+    
+    if modality == "business analyst":
+        formatted_response = format_business_analyst_response(formatted_response)
+    elif modality == "investigator":
+        formatted_response = format_investigator_response(formatted_response)
+    elif modality == "financial analyst":
+        formatted_response = format_financial_response(formatted_response)
 
+    return formatted_response
 
 if __name__ == "__main__":
     prompt = "your prompt here"
