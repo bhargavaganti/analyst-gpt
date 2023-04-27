b10d07a56db744dcd1a329ef826d3b8554b40e14
johncollinsai <climatemetrics@gmail.com>
2023-04-27 19:06:41 +0800

    v0.2.13, successfully merges responses with templates

--- app/completions.py
+++ app/completions.py
@@ -53,35 +53,45 @@ def extract_template_structure(template):
 
 def generate_gpt4_response(prompt, modality, api_key):
     if modality == "business analyst":
+        intro_sentence = "As a business analyst, I have identified the following key insights:"
         user_prompt = f"Please generate a response about {prompt} from the perspective of a business analyst. Start with this introduction sentence: '{intro_sentence}' Then list 10 key business analysis insights using bullet points. Format each bullet point as a new paragraph and ensure the content is relevant to the role of a business analyst."
         template = bullet_points
     elif modality == "investigator":
-        user_prompt = f"Please generate a response about {prompt} from the perspective of an investigator. Start with this introduction sentence: '{intro_sentence_investigator}' Then create a table with 3 columns: General Investigative Information, Corporate Information, and Compliance Information. Include 3 sentences in each column, and make sure the information is relevant to an investigator's perspective."
+        intro_sentence = "As an investigator, I have compiled the following data:"
+        user_prompt = f"Please generate a response about {prompt} from the perspective of an investigator. Start with this introduction sentence: '{intro_sentence}' Then create a table with 3 columns: General Investigative Information, Corporate Information, and Compliance Information. Include 3 sentences in each column, and make sure the information is relevant to an investigator's perspective."
         template = table_investigator
     elif modality == "financial analyst":
-        user_prompt = f"Please generate a response about {prompt} from the perspective of a financial analyst. Start with this introduction sentence: '{intro_sentence_financial}' Then create a table with 3 columns: Assets & Liabilities, Cashflows & Liquidity, and Key Financial Ratios. Include 3 sentences or data points in each column, and ensure the information is relevant to a financial analyst's perspective."
+        intro_sentence = "As a financial analyst, I have analyzed the financial aspects of the prompt:"
+        user_prompt = f"Please generate a response about {prompt} from the perspective of a financial analyst. Start with this introduction sentence: '{intro_sentence}' Then create a table with 3 columns: Assets & Liabilities, Cashflows & Liquidity, and Key Financial Ratios. Include 3 sentences or data points in each column, and ensure the information is relevant to a financial analyst's perspective."
         template = table_financial
     else:
         raise ValueError("Modality must be one of 'business analyst', 'investigator', or 'financial analyst'.")
 
     placeholders = extract_template_structure(template)
 
-    response = openai.Completion.create(
-        engine="text-davinci-002",
-        prompt=f"{user_prompt}",
-        max_tokens=1024,
+    response = openai.ChatCompletion.create(
+        model="gpt-4",
+        messages=[
+            {"role": "system", "content": f"You are a {modality}."},
+            {"role": "user", "content": user_prompt}
+        ],
+        max_tokens=1000,
         n=1,
         stop=None,
-        temperature=0.7,
-        frequency_penalty=0,
-        presence_penalty=0
+        temperature=0.3,  # Decrease temperature to make output more conservative
     )
 
-    formatted_response = response.choices[0].text.strip()
+    formatted_response = response.choices[0]["message"]["content"]  # works for v0.2.11
 
     # Replace the template placeholders with the generated content
     generated_content = formatted_response[len(intro_sentence):].strip()
-    response_parts = generated_content.split("•")[1:]
+    
+    # Split the generated content based on the line breaks
+    response_parts = generated_content.split("\n")
+    
+    # Remove any empty lines
+    response_parts = [part.strip() for part in response_parts if part.strip()]
+    
     filled_template = template
     for i, part in enumerate(response_parts):
         filled_template = filled_template.replace(placeholders[i], f"• {part.strip()}")
@@ -89,18 +99,3 @@ def generate_gpt4_response(prompt, modality, api_key):
     final_response = f"{intro_sentence}\n{filled_template}"
 
     return final_response
-
-if __name__ == "__main__":
-    prompt = "Enter company name here ..."
-    
-    print("Business Analyst:")
-    gpt4_analytical = generate_gpt4_response(prompt, "business analyst", api_key)
-    print(gpt4_analytical)
-
-    print("\nInvestigator:")
-    gpt4_investigative = generate_gpt4_response(prompt, "investigator", api_key)
-    print(gpt4_investigative)
-
-    print("\nFinancial Analyst:")
-    gpt4_financial = generate_gpt4_response(prompt, "financial analyst", api_key)
-    print(gpt4_financial)
